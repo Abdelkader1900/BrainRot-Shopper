d771212ac43c7387f9fcdce5d8f39cdec026f1af
abdelkader <abdelkader.lakhdar@epitech.eu>
2026-04-01 16:41:47 +0200

    Ajout du mode Nuit et custom navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,30 @@
+import { Button, Container, Flex, HStack, Text, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { PlusSquareIcon } from '@chakra-ui/icons'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+
+    const {colorMode, toggleColorMode} = useColorMode();
+
   return (
-    <div>Navbar</div>
+    <Container maxW={"1140px"} px={4} bg={useColorModeValue("gray.100", "gray.900")}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"} flexDir={{base:"column", sm:"row"}}>
+            <Text fontSize={{base:"22", sm:"28"}} fontWeight={"bold"} textTransform={"uppercase"} textAlign={"center"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
+                <Link to={"/"}>BrainRot Shop</Link>
+            </Text>
+            <HStack spacing = {2} alignItems={"center"}>
+                <Link to={"/create"}>
+                    <Button>
+                        <PlusSquareIcon fontSize={"20"} />
+                    </Button>
+                </Link>
+                <Button onClick={toggleColorMode}>
+                    {colorMode === "light" ? "Nuit" : "Jour"}
+                </Button>
+            </HStack>
+        </Flex>
+    </Container>
   )
 }
 
